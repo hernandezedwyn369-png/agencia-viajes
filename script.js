6cfe447a3236964755fd5066dc8681ff993e1fbb
hernandezedwyn369-png <hernandezedwyn369@gmail.com>
2026-06-12 22:27:03 -0600

    Proyecto listo para despliegue

---
+++ script.js
@@ -0,0 +1,155 @@
+const API_URL =
+"http://localhost:3000";
+
+let idEditar = null;
+
+async function cargarDestinos(){
+
+    const res =
+    await fetch(`${API_URL}/destinos`);
+
+    const datos =
+    await res.json();
+
+    renderizar(datos);
+}
+
+async function guardarDestino(){
+
+    const destino =
+    document.getElementById("destino").value;
+
+    const pais =
+    document.getElementById("pais").value;
+
+    const precio =
+    document.getElementById("precio").value;
+
+    const datos = {
+        destino,
+        pais,
+        precio
+    };
+
+    if(idEditar){
+
+        await fetch(
+            `${API_URL}/editar/${idEditar}`,
+            {
+                method:"PUT",
+                headers:{
+                    "Content-Type":"application/json"
+                },
+                body:JSON.stringify(datos)
+            }
+        );
+
+        idEditar = null;
+        document.getElementById("btnGuardar").innerText =
+        "Guardar Viaje";
+    }else{
+
+        await fetch(
+            `${API_URL}/guardar`,
+            {
+                method:"POST",
+                headers:{
+                    "Content-Type":"application/json"
+                },
+                body:JSON.stringify(datos)
+            }
+        );
+
+    }
+
+    document.getElementById("destino").value = "";
+    document.getElementById("pais").value = "";
+    document.getElementById("precio").value = "";
+
+    cargarDestinos();
+}
+
+async function buscarDestino(){
+
+    const termino =
+    document.getElementById("buscar").value;
+
+    const res =
+    await fetch(
+    `${API_URL}/buscar/${termino}`
+    );
+
+    const datos =
+    await res.json();
+
+    renderizar(datos);
+}
+
+async function eliminarDestino(id){
+
+    await fetch(
+    `${API_URL}/eliminar/${id}`,
+    {
+        method:"DELETE"
+    });
+
+    cargarDestinos();
+}
+
+function editarDestino(
+    id,
+    destino,
+    pais,
+    precio
+){
+
+    idEditar = id;
+
+    document.getElementById("destino").value = destino;
+    document.getElementById("pais").value = pais;
+    document.getElementById("precio").value = precio;
+
+    document.getElementById("btnGuardar").innerText="Actualizar viaje";
+}
+
+function renderizar(destinos){
+
+    const contenedor =
+    document.getElementById("contenedor");
+
+    contenedor.innerHTML =
+    destinos.map(v=>`
+
+        <div class="card">
+
+            <h3>🌎 ${v.destino}</h3>
+
+            <p><strong>País:</strong>
+            ${v.pais}</p>
+
+            <p><strong>Costo:</strong>
+            $${v.precio}</p>
+
+            <button onclick="editarDestino(
+            '${v._id}',
+            '${v.destino}',
+            '${v.pais}',
+            '${v.precio}'
+            )">
+            Editar
+            </button>
+
+            <button
+            class="btnEliminar"
+            onclick="eliminarDestino('${v._id}')">
+
+            Eliminar
+
+            </button>
+
+        </div>
+
+    `).join("");
+}
+
+cargarDestinos();
---
+++ server.js
@@ -0,0 +1,96 @@
+require('dotenv').config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Conexión MongoDB Atlas
+const mongoURI = process.env.MONGODB_URI;
+
+mongoose.connect(mongoURI)
+.then(() => console.log("Mongo conectado"))
+.catch(err => console.log(err));
+
+// Modelo
+const Viaje = mongoose.model(
+    "Viaje",
+    new mongoose.Schema({
+        destino: String,
+        pais: String,
+        precio: Number
+    })
+);
+
+// Mostrar todos
+app.get("/destinos", async (req, res) => {
+
+    const datos = await Viaje.find();
+
+    res.json(datos);
+
+});
+
+// Guardar
+app.post("/guardar", async (req, res) => {
+
+    const nuevo = new Viaje(req.body);
+
+    await nuevo.save();
+
+    res.json({
+        mensaje: "Guardado"
+    });
+
+});
+
+// Buscar
+app.get("/buscar/:destino", async (req, res) => {
+
+    const datos = await Viaje.find({
+        destino: {
+            $regex: req.params.destino,
+            $options: "i"
+        }
+    });
+
+    res.json(datos);
+
+});
+
+// Eliminar
+app.delete("/eliminar/:id", async (req, res) => {
+
+    await Viaje.findByIdAndDelete(req.params.id);
+
+    res.json({
+        mensaje: "Eliminado"
+    });
+
+});
+
+// Editar
+app.put("/editar/:id", async (req, res) => {
+
+    await Viaje.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+    );
+
+    res.json({
+        mensaje: "Actualizado"
+    });
+
+});
+
+// Puerto para Render
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor activo en puerto ${PORT}`);
+});
