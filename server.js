97811670bc21a8b8f47e95fbe9a4de8c35aacf87
hernandezedwyn369-png <hernandezedwyn369@gmail.com>
2026-06-13 21:24:42 -0600

    Mostrar index en Render

--- server.js
+++ server.js
@@ -3,11 +3,18 @@ require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(__dirname));
+
+app.get("/",(req, res) =>{
+    res.sendFile(path.join(__dirname, "index.html"));
+    });
 
 // Conexión MongoDB Atlas
 const mongoURI = process.env.MONGODB_URI;
